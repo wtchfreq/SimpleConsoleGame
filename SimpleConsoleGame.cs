9bbe3cb9b092f04dfb9c3d6533c3d306c9bac17c
Kostiantyn <kost.bogomazov@gmail.com>
2018-10-04 17:05:27 +0300

    Added simple game loop

---
+++ SimpleConsoleGame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleConsoleGame
+{
+    class SimpleConsoleGame
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Game Start!");
+            new SimpleConsoleGame().GameLoop();
+                        
+            
+        }
+
+        private void GameLoop()
+        {
+            ConsoleKeyInfo keyInfo;
+
+            while ((keyInfo = Console.ReadKey(true)).Key != ConsoleKey.Escape)
+            {
+                Console.WriteLine("Game loop is running");
+
+            }
+        }
+    }
+}
